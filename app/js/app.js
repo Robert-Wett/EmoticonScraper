769faeab5c910b75c797c9482a2636aa8e2bda2c
Robert Wettlaufer <rdwettlaufer@gmail.com>
2014-02-21 23:48:25 -0500

    Removing some boilerplate, adding teh socket lib

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,23 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('app', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
+  'app.filters',
+  'app.services',
+  'app.directives',
+  'app.controllers',
+  'socket'
+]);
+/*
+Check into this, but for now, just use the basics, not routing which 
+isn't defined at this point.
+
+
+.
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+ */
